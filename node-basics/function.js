a6ac29cf8ef181a53476555aa3b5c51acd264645
Ivan <ivankuzel@gmail.com>
2016-04-25 10:27:02 -0300

    basics

---
+++ node-basics/function.js
@@ -0,0 +1,7 @@
+function add (a, b){
+	return a + b;
+}
+
+var result = add(2,4);
+
+console.log(result);
---
+++ node-basics/variables.js
@@ -0,0 +1,4 @@
+var firstName = 'Ivan';
+var lastName = 'Kuzel';
+var fullName = firstName + ' ' + lastName;
+console.log (fullName);
